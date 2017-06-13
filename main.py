640b148e0714e7fe0641453bd3715a8596f24beb
gilmarf <gilmarf@localhost.localdomain>
2017-06-13 13:17:35 -0300

    le e visualiza a malha

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-import numpy
-from vtk import vtkStructuredPointsReader
-from vtk.util import numpy_support as VN
+# -*- coding: utf-8 -*-
 
-filename = "input/ugridex.vtk"
+import vtk
 
-reader = vtkStructuredPointsReader()
-reader.SetFileName(filename)
-print reader
+# The source file
+file_name = "input/data1_2D_scivis.vtk"
+
+# Read the source file.
+reader = vtk.vtkUnstructuredGridReader()
+reader.SetFileName(file_name)
+reader.Update()  # Needed because of GetScalarRange
+output = reader.GetOutput()
+scalar_range = output.GetScalarRange()
+
+# Create the mapper that corresponds the objects of the vtk file
+# into graphics elements
+mapper = vtk.vtkDataSetMapper()
+mapper.SetInput(output)
+mapper.SetScalarRange(scalar_range)
+
+# Create the Actor
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+
+# Create the Renderer
+renderer = vtk.vtkRenderer()
+renderer.AddActor(actor)
+renderer.SetBackground(0.5, 0.5, 0.5)  # Set background to white
+
+# Create the RendererWindow
+renderer_window = vtk.vtkRenderWindow()
+renderer_window.AddRenderer(renderer)
+
+# Create the RendererWindowInteractor and display the vtk_file
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetRenderWindow(renderer_window)
+interactor.Initialize()
+interactor.Start()
